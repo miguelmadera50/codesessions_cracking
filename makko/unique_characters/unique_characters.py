40d2ae3eca522c617507182fe881b68a9dc62e0f
Héctor Vela <hector.vela@wizeline.com>
2018-02-17 16:25:36 -0600

    add solution using data structures

--- makko/unique_characters/unique_characters.py
+++ makko/unique_characters/unique_characters.py
@@ -1,4 +1,4 @@
-def unique_characters(string_to_check):
+def unique_characters_no_aditional_structures(string_to_check):
     string_length = len(string_to_check)
     index = 0
     while index < string_length:
@@ -9,9 +9,21 @@ def unique_characters(string_to_check):
             if character == string_to_check[second_index]:
                 character_count += 1
                 if character_count > 1:
-                    print ('Nay: ', character, 'was found more than once')
+                    print('Nay: ', character, 'was found more than once')
                     return False
             second_index += 1
         index += 1
-    print ('Yay, the string has unique characters')
+    print('Yay, the string has unique characters')
+    return True
+
+
+def unique_characters_aditional_structures(string_to_check):
+    letters_checked = []
+    for character in string_to_check:
+        if character in letters_checked:
+            print('Nay, the character', character, 'was found more than once')
+            return False
+        else:
+            letters_checked.append(character)
+    print('Yay, the string has unique characters')
     return True
