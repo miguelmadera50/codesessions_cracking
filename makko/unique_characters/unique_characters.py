482addff9af7da845b4be357a59e0822c2a9ee95
Héctor Vela <hector.vela@wizeline.com>
2018-02-17 15:17:48 -0600

    unique characters

--- makko/unique_characters/unique_characters.py
+++ makko/unique_characters/unique_characters.py
@@ -3,15 +3,15 @@ def unique_characters(string_to_check):
     index = 0
     while index < string_length:
         character = string_to_check[index]
-        character_count = 0
+        character_count = 1
         second_index = index + 1
         while second_index < string_length:
             if character == string_to_check[second_index]:
                 character_count += 1
+                if character_count > 1:
+                    print ('Nay: ', character, 'was found more than once')
+                    return False
             second_index += 1
-        if character_count > 1:
-            print ('Nay: ', character, 'was found more than once')
-            return False
         index += 1
     print ('Yay, the string has unique characters')
     return True
