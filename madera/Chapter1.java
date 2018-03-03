2b74318bd6f439b89eb32a86e0cc785fa0b11f04
madera-wizeline <miguel.madera@wizeline.com>
2018-03-03 15:03:23 -0600

    Update with new tests

--- madera/Chapter1.java
+++ madera/Chapter1.java
@@ -7,12 +7,30 @@ class Chapter1 {
 
         // Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use
         // additional data structures?
-        String unique = "qwerty";
-        String not_unique = "qwyerty";
-        System.out.println("Problem 1a: " + problem1a(unique));
-        System.out.println("Problem 1a: " + !problem1a(not_unique));
-        System.out.println("Problem 1b: " + problem1b(unique));
-        System.out.println("Problem 1b: " + !problem1b(not_unique));
+
+        // Positive, w/ data structures
+        assert problem1a("");
+        assert problem1a("a");
+        assert problem1a("abc");
+        assert problem1a("qwertyuiopasdfghjklzxcvbnm");
+
+        // Negative, w/ data structures;
+        assert !problem1a("aa");
+        assert !problem1a("qwertyuiopasdfghjklzxcvbnq");
+        assert !problem1a("qwertyuiopasdfghjklzxcvbnzuythe");
+        assert !problem1a("qwertyhnbgfdsabzxc");
+
+        // Positive, wo/ data structures;
+        assert problem1b("");
+        assert problem1b("a");
+        assert problem1b("abc");
+        assert problem1b("qwertyuiopasdfghjklzxcvbnm");
+
+        // Negative, wo/ data structures;
+        assert !problem1b("aa");
+        assert !problem1b("qwertyuiopasdfghjklzxcvbnq");
+        assert !problem1b("qwertyuiopasdfghjklzxcvbnzuythe");
+        assert !problem1b("qwertyhnbgfdsabzxc");
     }
 
     // O(n^2) approach, simple iteration
